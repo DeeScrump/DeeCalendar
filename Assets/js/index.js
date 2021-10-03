081bfc9f03406a09487f3a3c34394aff9307e010
DCKII <dckornbluthii@bellsouth.net>
2021-10-02 21:34:37 -0400

    completed the html file

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,10 +1,15 @@
 var dayDisplayEl = $('#currentDay');
-var hourNineEl = $('#nineAM');
 var currentHour = moment().format('hA');
-var apptNineEl = $('#apptInfoNine');
-var apptTenEl = $('#apptInfoTen');
 
-var saveBtn = $('#saveIconTen');
+var hourNineEl = $('#nineAM');
+var hourTenEl = $('#tenAM');
+var hourElevenEl = $('#elevenAM');
+var hourTwelveEl = $('#twelvePM');
+var hourOneEl = $('#onePM');
+var hourTwoEl = $('#twoPM');
+var hourThreeEl = $('#threePM');
+var hourFourEl = $('#fourPM');
+var hourFiveEl = $('#fivePM');
 
 
 //code to display time under jumbotron
@@ -14,48 +19,7 @@ function dayDisplay() {
 }
 setInterval(dayDisplay, 1000);
 
-// function hourDisplay() {
+// function pastHourDisplay() {
+//   if (currentHour > )
 console.log(currentHour);
-// }
-// setInterval(hourDisplay);
-
-
-// if (dayDisplayEl == '5PM') {
-
-// }
-
-// function handleFormSubmit(event) {
-//     // Prevent the default behavior
-//     event.preventDefault();
-  
-//     console.log('First Name:', firstNameEl.val());
-//     console.log('Last Name:', lastNameEl.val());
-//     localStorage.setItem(apptNineEl)
-  
-//     // Select all checked options
-//     var checkedEl = $('input:checked');
-//     var selected = [];
-  
-//     // Loop through checked options to store in array
-//     $.each(checkedEl, function () {
-//       selected.push($(this).val());
-//     });
-//     console.log('Toppings: ', selected.join(', '));
-  
-//     // Clear input fields
-//     $('input[type="text"]').val('');
-//     $('input[type="email"]').val('');
-//     $('input[type="checkbox"]').prop('checked', false);
-//   }
-  
-//   // Submit event on the form
-//   formEl.on('submit', handleFormSubmit);
-
-  var disableInputs = function() {
-    sessionStorage.disableInputs = 'true';
-    document.$('firstname').disabled = true;
-    document.$('lastname').disabled = true;
-
-  };
-  if (sessionStorage.disableInputs === 'true') disableInputs();
-  document.$('saveBtn').onclick = disableInputs;
+console.log($('#nineAM .hour');
