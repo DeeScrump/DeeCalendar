4af3e6721341a70635c456e7e5462a84b277b6f2
DCKII <dckornbluthii@bellsouth.net>
2021-10-03 00:22:45 -0400

    got time comparator to work

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,15 +1,16 @@
 var dayDisplayEl = $('#currentDay');
-var currentHour = moment().format('hA');
+var currentHour = moment().format('HH');
 
-var hourNineEl = $('#nineAM');
-var hourTenEl = $('#tenAM');
-var hourElevenEl = $('#elevenAM');
-var hourTwelveEl = $('#twelvePM');
-var hourOneEl = $('#onePM');
-var hourTwoEl = $('#twoPM');
-var hourThreeEl = $('#threePM');
-var hourFourEl = $('#fourPM');
-var hourFiveEl = $('#fivePM');
+var hourNineEl = moment($('#nineAM .hour').text(), ['hA']).format('HH');
+var hourTenEl = moment($('#tenAM .hour').text(), ['hA']).format('HH');
+var hourElevenEl = moment($('#elevenAM .hour').text(), ['hA']).format('HH');
+var hourTwelveEl = moment($('#twelvePM .hour').text(), ['hA']).format('HH');
+var hourOneEl = moment($('#onePM .hour').text(), ['hA']).format('HH');
+var hourTwoEl = moment($('#twoPM .hour').text(), ['hA']).format('HH');
+var hourThreeEl = moment($('#threePM .hour').text(), ['hA']).format('HH');
+var hourFourEl = moment($('#fourPM .hour').text(), ['hA']).format('HH');
+var hourFiveEl = moment($('#fivePM .hour').text(), ['hA']).format('HH');
+var timeTable = [hourNineEl, hourTenEl, hourElevenEl, hourTwelveEl, hourOneEl, hourTwoEl, hourThreeEl, hourFourEl, hourFiveEl];
 
 
 //code to display time under jumbotron
@@ -20,6 +21,28 @@ function dayDisplay() {
 setInterval(dayDisplay, 1000);
 
 // function pastHourDisplay() {
-//   if (currentHour > )
-console.log(currentHour);
-console.log($('#nineAM .hour');
+  console.log(currentHour);
+  console.log(timeTable);
+
+
+
+$('.time-block').each(function(i) {
+  if (timeTable[i] < currentHour) {
+    $(this).addClass('past');
+  } else if (timeTable[i] === currentHour) {
+    $(this).removeClass('past');
+    $(this).addClass('present');
+  } else {
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+    $(this).addClass('future');
+  }
+});
+
+
+  // if (currentHour > moment().hourTwelveEl) {
+  //   console.log(hourNineEl + " is earlier in the day than " + currentHour);
+  // } else {
+  //   console.log('You are crazy!');
+  // }
+
